b588fd0951a25502229dc7115d4f995a07fa0296
wmendo8 <wmendo8@wgu.edu>
2026-01-23 07:38:14 -0800

    Initial Commit

---
+++ src/main/java/com/wgu/wallacebackend/WallacebackendApplication.java
@@ -0,0 +1,13 @@
+package com.wgu.wallacebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WallacebackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WallacebackendApplication.class, args);
+    }
+
+}
